fe3cbd65e60c7de7998818c5c789b40408f94aae
Uulkan Mamadalieva <41396709+kelya91@users.noreply.github.com>
2019-01-07 12:17:40 -0800

    edited

---
+++ config.py
@@ -0,0 +1,9 @@
+#Yelp.com
+client_id = "siej-rOqUcRUsJB4HrM-1Q"
+api_key = "7Rcjdq3xgPd9IZY6KWM5KYNXJV5KFjnWTYjPF26Qv7mM8eyfqT7B5FmOeblALI5diVyrHcrSPlEix98LbaJPf2CqdZmSJCPF1d9onXSWdjF1rAnLhTFiW9XMVn2xW3Yx"
+
+#Twitter.com
+consumer_key = "ysRrSi5HlC2FU2JyzhdfnDSYx"
+consumer_secret = "0pCWYfGu1P0cUEUNBYINnUaMvP2NaxPoEh0DJVzvMaAfxtVqYt"
+access_token = "343019560-GPiDShOMjuCLiHBECwxLk17H3RLW7mVIvfiQ37Oy"
+access_token_secret = "TA6nFftx6JupbUpx8XWWjekUw78sizNnKh3cEmGXqP55a"
